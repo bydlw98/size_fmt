9482b1137d1984faa3b3992e7a5304eb552d7cfa
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-06-23 16:45:48 +0800

    feat: add support for i64

--- src/tests.rs
+++ src/tests.rs
@@ -187,6 +187,188 @@ fn test_size_fmt_18_000_000_000_000_000_000_u64() {
     );
 }
 
+#[test]
+fn test_size_fmt_1000_i64() {
+    inner_test_size_fmt(1000_i64, "1000", "1000", "1.0k", "1000");
+}
+
+#[test]
+fn test_size_fmt_100_000_i64() {
+    inner_test_size_fmt(100_000_i64, "100000", "98K", "100k", "98Ki");
+}
+
+#[test]
+fn test_size_fmt_2_000_000_i64() {
+    inner_test_size_fmt(2_000_000_i64, "2000000", "2.0M", "2.0M", "2.0Mi");
+}
+
+#[test]
+fn test_size_fmt_200_000_000_i64() {
+    inner_test_size_fmt(200_000_000_i64, "200000000", "191M", "200M", "191Mi");
+}
+
+#[test]
+fn test_size_fmt_3_000_000_000_i64() {
+    inner_test_size_fmt(3_000_000_000_i64, "3000000000", "2.8G", "3.0G", "2.8Gi");
+}
+
+#[test]
+fn test_size_fmt_300_000_000_000_i64() {
+    inner_test_size_fmt(300_000_000_000_i64, "300000000000", "280G", "300G", "280Gi");
+}
+
+#[test]
+fn test_size_fmt_4_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        4_000_000_000_000_i64,
+        "4000000000000",
+        "3.7T",
+        "4.0T",
+        "3.7Ti",
+    );
+}
+
+#[test]
+fn test_size_fmt_400_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        400_000_000_000_000_i64,
+        "400000000000000",
+        "364T",
+        "400T",
+        "364Ti",
+    );
+}
+
+#[test]
+fn test_size_fmt_5_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        5_000_000_000_000_000_i64,
+        "5000000000000000",
+        "4.5P",
+        "5.0P",
+        "4.5Pi",
+    );
+}
+
+#[test]
+fn test_size_fmt_500_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        500_000_000_000_000_000_i64,
+        "500000000000000000",
+        "445P",
+        "500P",
+        "445Pi",
+    );
+}
+
+#[test]
+fn test_size_fmt_6_000_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        6_000_000_000_000_000_000_i64,
+        "6000000000000000000",
+        "5.3E",
+        "6.0E",
+        "5.3Ei",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_1000_i64() {
+    inner_test_size_fmt(-1000_i64, "-1000", "-1000", "-1.0k", "-1000");
+}
+
+#[test]
+fn test_size_fmt_negative_100_000_i64() {
+    inner_test_size_fmt(-100_000_i64, "-100000", "-98K", "-100k", "-98Ki");
+}
+
+#[test]
+fn test_size_fmt_negative_2_000_000_i64() {
+    inner_test_size_fmt(-2_000_000_i64, "-2000000", "-2.0M", "-2.0M", "-2.0Mi");
+}
+
+#[test]
+fn test_size_fmt_negative_200_000_000_i64() {
+    inner_test_size_fmt(-200_000_000_i64, "-200000000", "-191M", "-200M", "-191Mi");
+}
+
+#[test]
+fn test_size_fmt_negative_3_000_000_000_i64() {
+    inner_test_size_fmt(
+        -3_000_000_000_i64,
+        "-3000000000",
+        "-2.8G",
+        "-3.0G",
+        "-2.8Gi",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_300_000_000_000_i64() {
+    inner_test_size_fmt(
+        -300_000_000_000_i64,
+        "-300000000000",
+        "-280G",
+        "-300G",
+        "-280Gi",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_4_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        -4_000_000_000_000_i64,
+        "-4000000000000",
+        "-3.7T",
+        "-4.0T",
+        "-3.7Ti",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_400_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        -400_000_000_000_000_i64,
+        "-400000000000000",
+        "-364T",
+        "-400T",
+        "-364Ti",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_5_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        -5_000_000_000_000_000_i64,
+        "-5000000000000000",
+        "-4.5P",
+        "-5.0P",
+        "-4.5Pi",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_500_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        -500_000_000_000_000_000_i64,
+        "-500000000000000000",
+        "-445P",
+        "-500P",
+        "-445Pi",
+    );
+}
+
+#[test]
+fn test_size_fmt_negative_6_000_000_000_000_000_000_i64() {
+    inner_test_size_fmt(
+        -6_000_000_000_000_000_000_i64,
+        "-6000000000000000000",
+        "-5.3E",
+        "-6.0E",
+        "-5.3Ei",
+    );
+}
+
 fn inner_test_size_fmt<I: Integer>(
     size: I,
     raw_str: &str,
